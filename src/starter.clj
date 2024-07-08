e42dc13e2934820ad3af4c581bf1468881fcdad9
Andrzej Fricze <a.fricze@pm.me>
2024-07-08 21:59:21 +0200

    hn

--- src/starter.clj
+++ src/starter.clj
@@ -1,11 +1,85 @@
 (ns starter
   (:require
-    [io.github.humbleui.ui :as ui]))
+   [clojure.data.json :as json]
+   [clj-http.client :as client]
+   [clojure.java.browse :as browse]
+   [io.github.humbleui.ui :as ui]
+   [io.github.humbleui.signal :as signal]
+   [io.github.humbleui.window :as window]
+   [io.github.humbleui.font :as font]))
+
+(defonce *news
+  (signal/signal []))
+
+(defonce *loading
+  (signal/signal false))
+
+(defonce *window
+  (atom nil))
+
+(defn redraw!
+  "Requests a redraw on the next available frame."
+  []
+  (some-> @*window window/request-frame))
+
+
+
+(defn get-news []
+  (->> "https://hacker-news.firebaseio.com/v0/topstories.json"
+       client/get
+       :body
+       json/read-json))
+
+(defn get-news-item [id]
+  (->> (str "https://hacker-news.firebaseio.com/v0/item/" id ".json")
+       client/get
+       :body
+       json/read-json))
+
+(defn get-news-details []
+  (map get-news-item (get-news)))
+
+(nth (get-news-details) 8)
+
+(defn with-font []
+  (let [{:keys [face-ui scale]} ui/*ctx*
+        font (font/make-with-cap-height face-ui (* 12 scale))]
+    [ui/center
+     [ui/column {:gap 30}
+      [ui/align {:x :left}
+       [ui/row {:gap 10}
+        [ui/button
+         {:on-click (fn [_]
+                      (future
+                        (reset! *loading true)
+                        (reset! *news (take 10 (get-news-details)))
+                        (reset! *loading false)))}
+         [ui/label {:font font} "Fetch"]]
+        [ui/align
+         {:y :center}
+         [ui/label {:font font}
+          (if (deref *loading) "Loading..." "Loaded.")]]]]
+
+      [ui/column {:gap 20}
+       (map (fn [{:keys [title]}]
+              [ui/paragraph {:font font} title])
+            (deref *news))]]]))
+
 
 (ui/defcomp app []
-  [ui/center
-   [ui/label "Hello, world"]])
-  
+  [with-font])
+
+
+(redraw!)
+
+
 (defn -main [& args]
   (ui/start-app!
-    (ui/window #'app)))
+   (reset! *window
+           (ui/window
+            {:title    "HumbleUI Modal Example"
+             :bg-color 0xFFFFFFFF}
+            #'app))))
+
+
+#_(-main)
